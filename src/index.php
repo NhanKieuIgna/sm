096af0df8f993b9a6ed4340a865d33c984b63958
luannguyen <luantranedu99@gmail.com>
2025-11-23 12:48:13 +0700

    thêm cập nhật thông tin cá nhân cơ bản cho người dùng

--- src/index.php
+++ src/index.php
@@ -60,13 +60,13 @@ session_start() ?>
                 <a href="#" class="bell-icon"><img class="bell-icon-img" src="https://cdn-icons-png.flaticon.com/128/3602/3602145.png" alt="bell"></a>
                 <span>|</span>
                 <?php if(isset($_SESSION['user_id'])): ?>
-                    <span><u><?php echo $_SESSION['fullname']; ?></u></span>
+                    <span><u><a href="user/edit-profile.php"><?php echo $_SESSION['fullname']; ?></a></u></span>
                     <span>|</span>
                     <a href="logout.php"><u>Đăng Xuất</u></a>
                 <?php else: ?>    
-                <a href="/baitaplvn_sm/src/login.php"><u>Đăng nhập</u></a>
+                <a href="../src/login.php"><u>Đăng nhập</u></a>
                 <span>|</span>
-                <a href="/baitaplvn_sm/src/register.php"><u>Đăng ký</u></a>
+                <a href="../src/register.php"><u>Đăng ký</u></a>
                 <?php endif; ?>
                 <span>|</span>
                 <a href="#" class="cart-icon"><img class="cart-icon-img" src="https://cdn-icons-png.flaticon.com/128/1170/1170678.png" alt="cart"></a>
