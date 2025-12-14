4be645d8ef3f0c925c5d4776e1020cbbfada98a9
luannguyen <luantranedu99@gmail.com>
2025-12-14 22:10:45 +0700

    sua link

--- src/seller/header.php
+++ src/seller/header.php
@@ -62,7 +62,7 @@ if ($result_categories) {
                 <div class="category-menu">
                     <?php if (!empty($categories)): ?>
                         <?php foreach ($categories as $category): ?>
-                            <a href="user/list-product.php?category=<?php echo $category['ID_DanhMuc']; ?>"><?php echo htmlspecialchars($category['TenDanhMuc']); ?></a>
+                            <a href="../user/list-product.php?category=<?php echo $category['ID_DanhMuc']; ?>"><?php echo htmlspecialchars($category['TenDanhMuc']); ?></a>
                         <?php endforeach; ?>
                     <?php else: ?>
                         <a href="#">Chưa có danh mục</a>
